29dcb01a5087ad4d0e93cf14ada8130a80c1f24b
tetsuya-yamamoto-ai-learn <tetsuya.yamamoto.ai.study@gmail.ocm>
2019-10-09 10:47:19 +0900

    [課題N-2 Tensorの基本操作(basic1)]
    - tensorの配列操作を行いました。
    - 今のところ問題なし
    - Pycharmの使い方思い出したい

--- [N-1]Pytorch_basic1.py
+++ [N-1]Pytorch_basic1.py
@@ -1,5 +1,35 @@
-def basic1():
+import torch
 
+
+def basic1():
+    """
+    Tensorの作成
+    """
+    t1 = torch.tensor([[1,2],
+                      [3,4]])
+
+    print(t1)       # tensorの内容を表示
+    print(t1.dtype) # tensorの中身の型を表示(torch.int64型)
+
+    # 要素をfloatにすれば勝手にtorch.float32
+    t2 = torch.tensor([[1., 2.],[3., 4.]])
+    print(t2)
+    print(t2.dtype)
+
+    # ゼロ埋め
+    t3 = torch.zeros(size=(3, 2))
+    print(t3)
+    print(t3.dtype)
+
+    # 1埋め
+    t4 = torch.ones(size=(3, 2))
+    print(t4)
+    print(t4.dtype)
+
+    # 正規分布に従う乱数
+    t5 = torch.randn(size=(4, 3))
+    print(t5)
+    print(t5.dtype)
 
 if __name__ == '__main__':
     basic1()
